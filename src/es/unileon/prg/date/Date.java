e3804883713c343a66fa24b594a27f7da64ce5b8
Victor Fernandez Silva <vferns03@estudiantes.unileon.es>
2019-03-19 12:00:01 +0100

    Practica 1

--- src/es/unileon/prg/date/Date.java
+++ src/es/unileon/prg/date/Date.java
@@ -1,5 +1,39 @@
 package es.unileon.prg.date;
-public class Date{
+public class Date {
+
+	private int _year=1;
+	private int _month=1;
+	private int _day=1;
 	
+	public Date(int year, int month, int day) {
+		_year=year;
+		if (month > 0 && month<=12){
+			_month=month;
+		}
+		switch (_month){
+			case 2:{
+				if(day<=28){
+					_month=month;
+				}
+			}
+			case 1:		//next
+			case 3:		//next
+			case 5:		//next
+			case 7:		//next
+			case 8:		//next
+			case 10:	//next
+			case 11:{
+				if (day<=31){
+					_month=month;
+				}
+			}
+			default:{
+				if (day<=30){
+					_month=month;
+				}
+			}
+			
+		}
+	}
 
 }
